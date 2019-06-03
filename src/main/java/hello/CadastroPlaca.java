57a403711d3ba975c5b7e0e8126c366a7ffd1866
Lucas Angelo <lucas.angelosantana@hotmail.com>
2019-06-03 11:32:44 -0300

    commit

---
+++ src/main/java/hello/CadastroPlaca.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class CadastroPlaca {
+
+	private String placa;
+
+	public CadastroPlaca(String placa) {
+		super();
+		this.placa = placa;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	
+
+}
