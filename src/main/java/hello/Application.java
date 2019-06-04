6f3910553afdbf2e19b9b30c8518570809421980
Paloma Silva <palomaslima21@gmail.com>
2019-06-04 09:30:45 -0300

    add implementação do teste

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Application {
+	public static void main(String[] args) {
+		Placa placa = new Placa();
+		
+		placa.conferirPlaca("ABC1234");
+	}
+}
--- src/main/java/hello/Placa.java
+++ src/main/java/hello/Placa.java
@@ -1,5 +1,25 @@
 package hello;
 
+import java.util.ArrayList;
+
 public class Placa {
 	
+	
+	public boolean conferirPlaca(String placa) {
+		
+		ArrayList<String> placas = new ArrayList();
+		
+		placas.add("ABC1234");
+		
+		boolean retorno = false;
+		
+		for(String x : placas) {
+			if(x == placa) {
+				retorno = true;
+			} 
+		}
+		
+		return retorno;
+		
+	}
 }
