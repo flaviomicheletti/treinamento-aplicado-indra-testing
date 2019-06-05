c69e9acf28830080b08ae74f833deb58fdfde2f7
Lucas Angelo <lucas.angelosantana@hotmail.com>
2019-06-05 15:39:02 -0300

    criando classes pedagio, transportador e viagem

---
+++ src/main/java/hello/Pedagio.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Pedagio {
+
+	public Pedagio() {
+		
+	}
+
+	
+}
+
---
+++ src/main/java/hello/Viagem.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.util.ArrayList;
+
+public class Viagem {
+	
+	private Transportador transportador;
+	private String status;
+	private String tipo;
+	
+	ArrayList<Pedagio> listaPedagio = new ArrayList<>();
+	
+
+}
