a4d1e20f351d30a6bbacd1253a0602611f0479fc
Welerson <luizpawlak7@gmail.com>
2019-06-06 10:42:52 -0300

    Corrigindo teste 'getVeiculoByPlaca' e alterando dependencia em 'pom.xml'

--- src/test/java/hello/TransportadorTest.java
+++ src/test/java/hello/TransportadorTest.java
@@ -1,5 +1,17 @@
+package hello;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 public class TransportadorTest {
 
+    Transportador transportador = new Transportador();
+    Veiculo veiculo = new Veiculo("ABC1234", "Active", "0000");
+
+    @Test
+    public void getVeiculoByPlaca() {
+        assertThat(transportador.getVeiculoByPlaca("ABC1234"), samePropertyValuesAs(veiculo));
+    }
 }
