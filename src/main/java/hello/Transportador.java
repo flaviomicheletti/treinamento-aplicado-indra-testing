b7860e2802bdfda5965f74660852544d13cd9789
Welerson <luizpawlak7@gmail.com>
2019-06-06 10:19:37 -0300

    Adição do método 'getVeiculoByPlaca', criação do teste e adição de dependencia no Pom

--- src/main/java/hello/Transportador.java
+++ src/main/java/hello/Transportador.java
@@ -1,36 +1,32 @@
 package hello;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Transportador {
 
     private String idTransportador;
     private String nome;
-    private ArrayList<Veiculo> listaVeiculos;
+    private ArrayList<Veiculo> listaVeiculos = new ArrayList<Veiculo>();
 
+    //Construtor com dados mockados
     public Transportador() {
+        this.listaVeiculos.add(new Veiculo("ABC1234", "Active", "0000"));
     }
 
-    public String getIdTransportador() {
-        return idTransportador;
-    }
-    public void setIdTransportador(String idTransportador) {
-        this.idTransportador = idTransportador;
-    }
+    //Getter e Setter IdTransportados
+    public String getIdTransportador() { return idTransportador; }
+    public void setIdTransportador(String idTransportador) { this.idTransportador = idTransportador; }
 
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    //Getter e Setter Nome
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public ArrayList<Veiculo> getListaVeiculos() {
-        return listaVeiculos;
-    }
-    public void setListaVeiculos(ArrayList<Veiculo> listaVeiculos) {
-        this.listaVeiculos = listaVeiculos;
-    }
+    //Getter e Setter ListaVeiculos
+    public List<Veiculo> getListaVeiculos() { return listaVeiculos; }
+    public void setListaVeiculos(ArrayList<Veiculo> listaVeiculos) { this.listaVeiculos = listaVeiculos; }
 
     //Método pare retornar um veículo selecionado pela placa
     public Veiculo getVeiculoByPlaca(String placaProcurada){
